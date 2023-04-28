7e118e9d7bef98c5e410a2c3e153b83b1249cb79
Toka :) <131876053+Tokaref@users.noreply.github.com>
2023-04-28 16:06:58 +0200

    Update README.m

--- README.m
+++ README.m
@@ -1,12 +1,11 @@
 
-# Salut👋, je suis Toka
+Salut👋, je suis Toka
 
-🔥 **Passionné par l'info en général**
+🔥 Passionné par l'info en général
 
-📚 **Pour me contacte**r ⬇️
+📚*Pour me contacte ⬇️
 
-- **Mon discord : Toka !!#0001**
+Mon discord : Toka !!#0001
 
 
 
-![logo](https://i.pinimg.com/236x/44/b8/3c/44b83c691eff2e0f31ef5e5a80088dd6.jpg)
